07051e238d3514f377d309e292099c494660ec65
DioneDw <dionewilly@hotmail.com>
2022-12-15 12:27:06 -0300

    first class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+    Position position = new Position(3,2);
+    System.out.println(position);
 	}
 
 }
